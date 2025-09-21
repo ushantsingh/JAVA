4951c65662a2718959d5260876ad806afd581fb4
Ushant singh <ushantsingh@gmail.com>
2025-09-22 00:24:10 +0530

    PATTERNS

---
+++ pattern1.java
@@ -0,0 +1,22 @@
+// SOLID RECTANGLE
+
+// *****
+// *****
+// *****
+// *****
+
+public class pattern1{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<4;i++)
+        {
+            // System.out.println("*");
+        
+            for(int j=0;j<5;j++)
+             {
+                  System.out.print("*");
+             }
+             System.out.println();
+        }
+    }
+}
---
+++ pattern4.java
@@ -0,0 +1,18 @@
+// LOWER LEFT TRIANGLE
+
+
+
+public class pattern4{
+    public static void main(String[] args)
+    {
+        int n=4;
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=n;j>=i;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
